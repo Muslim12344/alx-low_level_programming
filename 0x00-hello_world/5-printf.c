9580e5744275fa0dc8de165558848ef57b60c133
Muslim12344 <bnaniothmane@gmail.com>
2023-06-15 06:47:06 -0700

    Allé

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts funcyion
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
